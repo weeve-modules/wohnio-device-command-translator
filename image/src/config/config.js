24cb5f4119ba1d93b2cd794372860f865d7adc4a
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-04-12 14:07:41 +0200

    initial build for generic translator module

---
+++ image/src/config/config.js
@@ -0,0 +1,8 @@
+const env = require('../utils/env')
+
+module.exports = {
+  HOST_NAME: env('HOST_NAME', '127.0.0.1'),
+  HOST_PORT: env('HOST_PORT', '8080'),
+  MODULE_NAME: env('MODULE_NAME', 'Generic Translator Module'),
+  EGRESS_URL: env('EGRESS_URL', ''),
+}
