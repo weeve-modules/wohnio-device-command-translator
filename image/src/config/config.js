ba831064bc8e7653fb433fa21da190a5fa3d2adb
Jakub Grzelak <60741674+kubagrzelak@users.noreply.github.com>
2022-04-28 15:08:56 +0100

    Update config.js

--- image/src/config/config.js
+++ image/src/config/config.js
@@ -3,6 +3,6 @@ const env = require('../utils/env')
 module.exports = {
   INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
   INGRESS_PORT: env('INGRESS_PORT', '8081'),
-  MODULE_NAME: env('MODULE_NAME', 'Generic Translator Module'),
+  MODULE_NAME: env('MODULE_NAME', 'generic-translator'),
   EGRESS_URL: env('EGRESS_URL', ''),
 }
